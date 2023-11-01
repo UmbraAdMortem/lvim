74d06a0d748f9938b85be715dbe956a6b63b32ab
Raphael <44473923+UmbraAdMortem@users.noreply.github.com>
2023-11-01 13:30:11 +0100

    Initial configuration

---
+++ config.lua
@@ -0,0 +1,7 @@
+require("user.options")
+require("user.keymappings")
+reload("user.plugins")
+
+lvim.colorscheme = "gruvbox-baby"
+
+
---
+++ lua/user/keymappings.lua
@@ -0,0 +1,24 @@
+-- Quick Options - change options quickly
+lvim.builtin.which_key.mappings["o"] = {
+  name = "Quick Options",
+  n = { "<cmd>setlocal relativenumber!<cr>", "Toggle relative line numbers"},
+}
+
+-- Quickly exit insert mode
+lvim.keys.insert_mode["jk"] = "<ESC>"
+lvim.keys.insert_mode["kj"] = "<ESC>"
+
+-- Save current buffer
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.insert_mode["<C-s>"] = "<ESC>:w<cr>i"
+lvim.keys.visual_mode["<C-s>"] = ":w<cr>"
+lvim.keys.visual_block_mode["<C-s>"] = ":w<cr>"
+
+-- Save all buffers
+lvim.keys.normal_mode["<C-S>"] = ":wa<cr>"
+lvim.keys.insert_mode["<C-S>"] = "<ESC>:wa<cr>i"
+lvim.keys.visual_mode["<C-S>"] = ":wa<cr>"
+lvim.keys.visual_block_mode["<C-S>"] = ":wa<cr>"
+lvim.keys.term_mode["<C-S>"] = ":wa<cr>"
+lvim.keys.command_mode["<C-S>"] = ":wa<cr>"
+

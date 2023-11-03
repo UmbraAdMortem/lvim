fd64f4295a2ceb63e1aca9a03b25b237d90edd88
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2023-11-03 13:58:15 +0100

    Added formater for css files

--- config.lua
+++ config.lua
@@ -4,4 +4,10 @@ reload("user.plugins")
 
 lvim.colorscheme = "gruvbox-baby"
 
-
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup {
+  {
+    name = "prettier",
+    filetypes = { "css", "typescript", "typescriptreact", "MarkDown" }
+  }
+}
